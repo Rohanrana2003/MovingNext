d3ca5acb1d47d21b2d854295e40363d6a5c5615f
Rohan rana <rohanrana33042@gmail.com>
2025-05-04 17:00:22 +0530

    refactor: clean up layout component and fix blog name reference

--- learn-next/app/blogs/[blogName]/page.js
+++ learn-next/app/blogs/[blogName]/page.js
@@ -1,10 +1,11 @@
 export default async function page({ params, searchParams }) {
-  console.log(await params);
-  console.log(await searchParams);
+  // console.log(await params);
+  // console.log(await searchParams);
+  var { blogName } = await params;
 
   return (
     <div>
-      <h1>Blog on {params.blogName}</h1>
+      <h1>Blog on {blogName}</h1>
     </div>
   );
 }
