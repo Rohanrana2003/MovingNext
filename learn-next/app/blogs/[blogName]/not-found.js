7f86ef7f52f7577317425544ef0f17651b2e76c0
Rohan rana <rohanrana33042@gmail.com>
2025-05-05 16:28:30 +0530

    feat: add not found page for blogs and validate blog name format

---
+++ learn-next/app/blogs/[blogName]/not-found.js
@@ -0,0 +1,8 @@
+export default function blogNotFound() {
+  return (
+    <div>
+      <h1>Blog Page Not Found</h1>
+      <p>Could not find the page you are looking for</p>
+    </div>
+  );
+}
--- learn-next/app/blogs/[blogName]/page.js
+++ learn-next/app/blogs/[blogName]/page.js
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default async function page({ params, searchParams }) {
   // console.log(await params);
   // console.log(await searchParams);
   var { blogName } = await params;
 
+  if (!/^\d+$/.test(blogName)) {
+    notFound();
+  }
   return (
     <div>
       <h1>Blog on {blogName}</h1>
